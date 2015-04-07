465ee5a80a99df71b725c725008591472ffd2eba
hgagnon <hgagnon@10.10.31.211>
2015-04-07 13:04:55 -0400

    Updated Main to include one GUI

--- Wristband/src/client/Main.java
+++ Wristband/src/client/Main.java
@@ -11,28 +11,12 @@ public class Main {
     public static void main(String[] args) {
         Baselining baseline = new Baselining();
         
-        /* Create Buttons for starting/stopping learning/training sessions */
-        JButton learningButton = new JButton("Start Learning Phase");
+        JButton learningButton = new JButton("Start Baselining Phase");
         learningButton.setBounds(50,10,300,50);
-        JButton trainingButton = new JButton("Start Training Phase");
+        JButton trainingButton = new JButton("Start Learning Phase");
         trainingButton.setBounds(400,10,300,50);
-        
-        /* Summary Link Button */
         JButton summaryButton = new JButton("Summary");
         summaryButton.setBounds(550,170,200,50);
-        
-        /* TO BE DELETED AFTER WE SELECT A SINGLE GUI TO USE */
-        JButton learningButton2 = new JButton("Start Timed Learning Phase");
-        learningButton2.setBounds(50,10,300,50);
-        JButton trainingButton2 = new JButton("Start Timed Training Phase");
-        trainingButton2.setBounds(400,10,300,50);
-        JButton summaryButton2 = new JButton("Summary");
-        summaryButton2.setBounds(550,170,200,50);
-        
-        /* Create Integer Text Box for adjusting set Session Time */
-        SpinnerModel model = new SpinnerNumberModel(2, .5, 5, .5);     
-        JSpinner sessionTimeSpinner = new JSpinner(model);
-        sessionTimeSpinner.setBounds(710, 10, 60, 50);
         
         /* Create Slider for adjusting Threshold */
         JSlider thresholdControl = new JSlider(JSlider.HORIZONTAL,0,100,50);
@@ -42,14 +26,8 @@ public class Main {
         thresholdControl.setPaintTicks(true);
         thresholdControl.setPaintLabels(true);
         
-        /* Create Integer Text Box for adjusting Threshold */
-        model = new SpinnerNumberModel(50, 1, 100, 1);     
-        JSpinner thresholdSpinner = new JSpinner(model);
-        thresholdSpinner.setBounds(10, 70, 700, 50);
-        
         
         Panel panel = new Panel(new JFrame(), baseline);
-        Panel panel2 = new Panel(new JFrame(), baseline);
         
         panel.setLayout(null);
         panel.add(learningButton);
@@ -57,18 +35,9 @@ public class Main {
         panel.add(thresholdControl);
         panel.add(summaryButton);
         
-        
-        panel2.setLayout(null);
-        panel2.add(learningButton2);
-        panel2.add(trainingButton2);
-        panel2.add(thresholdSpinner);
-        panel2.add(sessionTimeSpinner);
-        panel2.add(summaryButton2);
-        
         try {while (true) {
             baseline.updateData();
             panel.repaint();
-            panel2.repaint();
             synchronized (baseline) {baseline.wait(100);}
         }} catch (Exception e) {
             e.printStackTrace();
