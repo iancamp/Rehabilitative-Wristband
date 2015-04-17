e07979a1004815e72498f23628219f1eceb886da
iancamp <iancamp222@gmail.com>
2015-04-16 21:14:17 -0400

    Added a run hook script which will check if arduino system modifications have been made. If so, it will run the program. If not, it will call the install script. This script is incomplete since we do not yet know the name of the executable.

---
+++ Wristband/src/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+
+#check and see if the system has been configured to interface with an arduino properly
+if [ -d /var/lock -o \
+$(ls -l /var/lock | awk -v col=4 '{print $col}') = "uucp" -o \
+$(stat -c "%a %b" /var/lock) = "775" -o \
+-z $(dscl . -read /Groups/mygroup GroupMembership | grep -o $USER) ] then
+	/usr/bin/osascript -e 'do shell script "./install.sh" with administrator privileges'
+fi
+
+#need to insert "open" command statement here once we know name of executable
+
+exit 0;
+	
