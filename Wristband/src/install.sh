5ab78d3dcc0129b426173c03fa613c39cdd1f231
iancamp <iancamp222@gmail.com>
2015-04-21 14:12:36 -0400

    bug fixes in install and run scripts.

--- Wristband/src/install.sh
+++ Wristband/src/install.sh
@@ -16,12 +16,12 @@ if ! [[ $(ls -l /var/lock | awk -v col=4 '{print $col}') = "uucp" ]]; then
 fi
 
 #Change permissions on /var/lock to 775 if it is not 775
-if ! [[ $(stat -c "%a %b" /var/lock) = "775" ]]; then
+if ! [[ $(stat -f %Mp%Lp /var/lock) = "0775" ]]; then
 	echo "chmod 775 /var/lock"
 fi
 
 #add the user to the uucp group if they are not in it
-if ! [[ -z $(dscl . -read /Groups/mygroup GroupMembership | grep -o $USER1) ]]; then
+if [[ -z $(dscl . -read /Groups/_uucp GroupMembership | grep -o "$USER1") ]]; then
 	echo "dscl . -append /groups/_uucp GroupMembership $USER1"
 fi
 
--- Wristband/src/run.sh
+++ Wristband/src/run.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 #
 
 #check and see if the system has been configured to interface with an arduino properly
-if ! [[ -d /var/lock -o \
-$(ls -l /var/lock | awk -v col=4 '{print $col}') = "uucp" -o \
-$(stat -c "%a %b" /var/lock) = "775" -o \
--z $(dscl . -read /Groups/mygroup GroupMembership | grep -o $USER) ]]; then
-	/usr/bin/osascript -e 'do shell script "./install.sh $USER" with administrator privileges'
+if ! [[ -d /var/lock || \
+$(ls -l /var/lock | awk -v col=4 '{print $col}') = "uucp" || \
+$(stat -f %Mp%Lp /var/lock) = "0775" || \
+! -z $(dscl . -read /Groups/_uucp GroupMembership | grep -o $USER) ]]; then
+	echo "Now calling installation script"
+	#/usr/bin/osascript -e 'do shell script "./install.sh $USER" with administrator privileges'
 fi
 
 #need to insert "open" command statement here once we know name of executable
