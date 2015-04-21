c21988de12c676e0c5c7466ef13d5ec2df684b3a
iancamp <iancamp222@gmail.com>
2015-04-21 12:53:17 -0400

    Fixed a bug in the run script and added a test install script. Right now the install script will only print the commands it will run upon release rather than actually running them.

---
+++ Wristband/src/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#make necessary changes to the file system so the host interfaces with an arduino correctly
+
+USER1 = $1
+
+echo "$USER1"
+
+#create /var/lock directory if it doesn't exist
+if ! [[ -d /var/lock ]]; then
+	echo "mkdir /var/lock"
+fi
+
+#Change group permissions on /var/lock to uucp
+if ! [[ $(ls -l /var/lock | awk -v col=4 '{print $col}') = "uucp" ]]; then
+	echo "chgrp uucp /var/lock"
+fi
+
+#Change permissions on /var/lock to 775 if it is not 775
+if ! [[ $(stat -c "%a %b" /var/lock) = "775" ]]; then
+	echo "chmod 775 /var/lock"
+fi
+
+#add the user to the uucp group if they are not in it
+if ! [[ -z $(dscl . -read /Groups/mygroup GroupMembership | grep -o $USER1) ]]; then
+	echo "dscl . -append /groups/_uucp GroupMembership $USER1"
+fi
+
+exit 0;
+	
+	
