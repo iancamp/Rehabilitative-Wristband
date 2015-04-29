37a4bcf257ba83b2306a76fef854ba8f14afcc5c
David <dmmotter@udel.edu>
2015-04-29 13:36:12 -0400

    Modified PlayGymToys code to work under the new system to receive messages from the software. Implementation is currently untested and needs compilation.

--- arduino/modified_src/PlayGymToys/PlayGymToys.ino
+++ arduino/modified_src/PlayGymToys/PlayGymToys.ino
@@ -14,7 +14,7 @@ uno controlling the play gym toys
 SoftwareSerial XBee(2, 3); // RX, TX
 const int motorPin = 9;  //Pin 9 controls the butterfly mobile
 const int musicPin = 10;  //Pin 10 controls the music
-const int ledPin = 8;  //Pin 8 controls the LEDs in the giraff 
+const int ledPin = 8;  //Pin 8 controls the LEDs in the giraff
 double noMoveCount = 0; //Total count of no movements recorded
 double lowMoveCount = 0; //Total count of low movements recorded
 double highMoveCount = 0;  //Total count of high movemnets recorded
@@ -24,13 +24,14 @@ double lowMoveAvg = 0;  //Avg of low movements through single session
 double highMoveAvg = 0; //Avg of high movements through single session
 String input = ""; //Input string to store the character bytes received so far.
 String prefix = String("arduts"); //Unique prefix which is used by computer to identify the arduino.
-int threshold = 50; //Threshold at which the device currently spins and turns on.
+int threshold = 9999; //Threshold at which the device currently spins and turns on. Device should never activate until learning phase, and so starting threshold is very high.
+String ack = String("ack");
 
 void setup()
 {
-  //Set music, motor, and led pins to outputs 
+  //Set music, motor, and led pins to outputs
   //Set both the XBee and serial ports at 9600 baud
-  pinMode(musicPin, OUTPUT); 
+  pinMode(musicPin, OUTPUT);
   pinMode(motorPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
   XBee.begin(9600);
@@ -40,70 +41,40 @@ void setup()
 void loop()
 {
   analogWrite(ledPin, 130); // always have leds turned on
-  
-  if (XBee.available() > 0)
-  { // If data comes in from XBee, send it out to serial monitor
-    while (XBee.available() > 0){
-      int inchar = XBee.read(); 
-      if (isDigit(inchar)){
-        input+= (char)inchar; //Read in the bytes of the number sent
-      }
-      if (inchar == '\n'){ //Find the null terminator
-        int inint = input.toInt();
-        Serial.println(prefix + input); //Send across the data with the unique prefix
-        //Now must determine if the received value goes over the threshold.
-        if (inint >= threshold){
-          analogWrite(motorPin, 255); //turns the motor on to maximum speed
-          digitalWrite(musicPin, HIGH); //turns the music on
-        }
-        else {
-          analogWrite(motorPin, 0); //turns the motor off
-          digitalWrite(musicPin, LOW); //turns the music off
-        }
-        input = ""; //Reset input String
-      }
+
+  // If data comes in from XBee, send it out to serial monitor
+  while (XBee.available() > 0) {
+    int inchar = XBee.read();
+    if (isDigit(inchar)) {
+      input += (char)inchar; //Read in the bytes of the number sent
     }
-    
-    
-    /*The data coming from the XBee is a Hexidecimal number corresponding
-    to the 3, 1 or 4 being sent from the cuff
-    */
-    //if(input == 51)//High Movement(3 from cuff)
-    //{
-     // analogWrite(motorPin, 255); //turns the motor on to maximum speed
-      //digitalWrite(musicPin, HIGH); //turns the music on
-      //highMoveCount = highMoveCount + 1; //increments the num of high movements
-      //totalDataCount = totalDataCount + 1; //increments the num of total movements
-    //}
-    //else if(input == 49)//Low Movement(1 from cuff)
-    //{
-      //analogWrite(motorPin, 200); //turns the motor on not at full speed
-      //lowMoveCount = lowMoveCount + 1; //increments the num of low movements
-      //totalDataCount = totalDataCount + 1; //increments the num of total movements
-    //}
-    //else if(input == 52)//No Movement(4 from cuff)
-    //{
-      //analogWrite(motorPin, 0); //turns the motor off
-      //digitalWrite(musicPin, LOW); //turns the music off
-      //noMoveCount= noMoveCount + 1; //increments the num of no movements
-      //totalDataCount = totalDataCount + 1; //increments the num of total movements
-    //}
-    
-  //noMoveAvg = (noMoveCount/totalDataCount)*100; //gets avg of no movements through session
-  //lowMoveAvg = (lowMoveCount/totalDataCount)*100; //gets avg of low movements through session
-  //highMoveAvg = (highMoveCount/totalDataCount)*100; //gets avg of high movements through session
-  
-  //Prints in one line the avg of no, low and high movements to serial monitor
-  //Format: No Movement: ##%  Low Movement: ##%  High Movement: ##%
-  //Serial.print("No Movement: ");
-  //Serial.print(noMoveAvg);
-  //Serial.print("%");
-  //Serial.print("\tLow Movement: ");
-  //Serial.print(lowMoveAvg);
-  //Serial.print("%");
-  //Serial.print("\tHigh Movement: ");
-  //Serial.print(highMoveAvg);
-  //Serial.println("%");
+    if (inchar == '\n') { //Find the null terminator
+      int inint = input.toInt();
+      Serial.println(prefix + input); //Send across the data with the unique prefix
+      //Now must determine if the received value goes over the threshold.
+      if (inint >= threshold) {
+        analogWrite(motorPin, 255); //turns the motor on to maximum speed
+        digitalWrite(musicPin, HIGH); //turns the music on
+      }
+      else {
+        analogWrite(motorPin, 0); //turns the motor off
+        digitalWrite(musicPin, LOW); //turns the music off
+      }
+      input = ""; //Reset input String
+    }
   }
   
+  while (Serial.available() > 0) {
+    int inchar = Serial.read();
+    if (isDigit(inchar) && inchar != '\n') {
+      input += (char)inchar; //Read in the bytes of the number sent
+    }
+    if (inchar == '\n') { //Find the null terminator
+      threshold = input.toInt();
+      Serial.println(ack + input + '\n'); //Send across the data with the unique ack prefix
+      input = ""; //Reset input String
+    }
+  }
+
+
 }
