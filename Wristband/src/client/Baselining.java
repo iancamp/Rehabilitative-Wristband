ec6dee13cd2177f9fd78bf276e5f76e1b6294d46
bnsavage <magictrev640@yahoo.com>
2015-04-30 20:54:18 -0400

    Baseline phase is WAY different now

--- Wristband/src/client/Baselining.java
+++ Wristband/src/client/Baselining.java
@@ -12,7 +12,8 @@ import java.util.Collections;
  *
  */
 public class Baselining {
-    private LinkedList<DataPoint> sessionData;
+    private LinkedList<DataPoint> baselineData;
+    private LinkedList<DataPoint> learningData;
     private NetworkThread wristbandInterface;
     //DataPoint newPoint;
     private float baseline;
@@ -21,12 +22,18 @@ public class Baselining {
     private float max;
     private double timerem;
     private int outliers;
+    private boolean startBaseline;
+    private double startTime;
+    private double timeinphase;
+    private double minutes;
+
 
     /**
      * Constructor to create a Baselining object
      */
     public Baselining(){
-        sessionData = new LinkedList<DataPoint>();
+        baselineData = new LinkedList<DataPoint>();
+        learningData = new LinkedList<DataPoint>();
         wristbandInterface = new NetworkThread();
         wristbandInterface.start();
         sum = 0;
@@ -83,7 +90,7 @@ public class Baselining {
      * @return The last data point
      */
     public DataPoint getLastPoint(){
-        return sessionData.getLast();
+        return baselineData.getLast();
     }
 
 
@@ -94,8 +101,8 @@ public class Baselining {
      */
     public static Baselining generateTestBasline(){
         Baselining testBaseline = new Baselining();
-        testBaseline.sessionData.add(new DataPoint(1,1));
-        testBaseline.sessionData.add(new DataPoint(2,2));
+        testBaseline.baselineData.add(new DataPoint(1,1));
+        testBaseline.baselineData.add(new DataPoint(2,2));
         //testBaseline.newPoint = new DataPoint(2,2);
         testBaseline.sum = (float) 3;
         testBaseline.baseline = (float) 1.5;
@@ -104,17 +111,7 @@ public class Baselining {
         return testBaseline;
     }
 
-    /**
-     * Collects the most recent data from the wristband then sorts the list 
-     * of new data
-     */
-   /* public void addNewDataToSessionList(){
-        LinkedList<DataPoint> newData = new LinkedList<DataPoint>();
-      
-        wristbandInterface.copyFromQueue(newData);
-        Collections.sort(newData);
-        
-    }    */
+
 
     /**
      * Adds the sum of a list to the baseline's sum and if any point's magnitude is larger than max, sets max to that magnitude
@@ -135,8 +132,8 @@ public class Baselining {
      * Getter for LinkedList of session data
      * @return A linked list of the data points collected in the current session
      */
-    public LinkedList<DataPoint> getSessionData(){
-        return sessionData;
+    public LinkedList<DataPoint> getbaselineData(){
+        return baselineData;
     }
 
     /**
@@ -145,14 +142,17 @@ public class Baselining {
      */
     public void updateData(){
         LinkedList<DataPoint> temporaryNewData = new LinkedList<DataPoint>();
-
         wristbandInterface.copyFromQueue(temporaryNewData);
-        //System.out.println(temporaryNewData.size());
+        if(startBaseline && ((System.currentTimeMillis()-startTime)< timeinphase)){
+            updateSumMax(temporaryNewData);
+            baselineData.addAll(temporaryNewData);
+            baseline = sum/(baselineData.size() - outliers);
+            timerem =  (minutes - ((System.currentTimeMillis() - startTime)/timeinphase));
+        }
 
-        updateSumMax(temporaryNewData);
 
-        sessionData.addAll(temporaryNewData);
-        baseline = sum/(sessionData.size() - outliers);
+
+
 
     }
 
@@ -162,9 +162,9 @@ public class Baselining {
      */
     public void himedlo(LinkedList<DataPoint> data){
         for(DataPoint currentpoint : data){
-            if(currentpoint.getMagnitude() <= (.25*max)){currentpoint.himedlo = "Low";}
-            else if(currentpoint.getMagnitude() <= (.75 * max)){currentpoint.himedlo = "Medium";}
-            else{currentpoint.himedlo = "High";}
+            if(currentpoint.getMagnitude() <= 15.0){currentpoint.himedlo = "Low";}
+            else if(15.0 < currentpoint.getMagnitude() && currentpoint.getMagnitude() <= threshold){currentpoint.himedlo = "Medium";}
+            else if(currentpoint.getMagnitude() > threshold){currentpoint.himedlo = "High";}
         }}
 
     /**
@@ -172,39 +172,32 @@ public class Baselining {
      * @param number of minutes, how long the learning phase will be, and the threshold for the learning phase
      * @return The baselining for the phase
      */
-    public Baselining baselinePhase(double minutes){
+    public void baselinePhase(double minutes){
         wristbandInterface.resetTime();
-        Baselining session = new Baselining();
+        timeinphase = (minutes*60*1000);
+        timerem = minutes;
+        startTime = System.currentTimeMillis();
+    }
+
+    /**
+     * Activates toy when movement exceeds threshold.
+     * @param minutes for the learning phase
+     */
+    public void learningPhase(double minutes){
         LinkedList<DataPoint> tempdata = new LinkedList<DataPoint>();
-        session.wristbandInterface.copyFromQueue(tempdata);
-        long timeinphase = (long) (minutes*60*1000);
-        timerem = (long) minutes;
+        wristbandInterface.copyFromQueue(tempdata);
+        double timeinphase = (minutes*60*1000);
+        timerem = minutes;
         long startTime = System.currentTimeMillis();
-        while((System.currentTimeMillis()-startTime)< timeinphase){
-            timerem =  (minutes - ((System.currentTimeMillis() - startTime)/timeinphase));
-            session.updateData();
-        }
-        return session;
-    }
-
-    /**
-     * Activates toy when movement exceeds threshold.  Also increases threshold when hit too many times.
-     * @param minutes for each learningphase, number of phases, and current threshold
-     */
-    public void learningPhase(double minutes, int phasecount, double threshold) {
-        while (phasecount != 0) {
+        while((System.currentTimeMillis() - startTime) < timeinphase){
+            timerem = (minutes - ((System.currentTimeMillis() - startTime)/timeinphase));
+            LinkedList<DataPoint> newdata = new LinkedList<DataPoint>();
+            wristbandInterface.copyFromQueue(newdata);
 
         }
     }
 
-    /**
-     * Calculates the current baseline
-     * @param magnitude
-     */
-    /*public void changeBaseline(float magnitude){
-        sum += magnitude;
-        baseline = sum/sessionData.size();
-    }*/
+
 
 }
  
