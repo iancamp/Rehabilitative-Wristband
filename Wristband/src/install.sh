944cf1a5217c03f8b40bc69634ab0a1f7f535b61
iancamp <iancamp222@gmail.com>
2015-05-14 20:08:45 -0400

    changed the install script to actually execute commands instead of just printing what would be executed

--- Wristband/src/install.sh
+++ Wristband/src/install.sh
@@ -8,24 +8,27 @@ echo "$USER1"
 
 #create /var/lock directory if it doesn't exist
 if [[ ! -d /var/lock ]]; then
-	echo "mkdir /var/lock"
+	echo "creating /var/lock"
+	mkdir /var/lock
 fi
 
 #Change group permissions on /var/lock to uucp
 if [[ ! $(ls -l /var/ | grep lock | awk -v col=4 '{print $col}') = "_uucp" ]]; then
-	echo "chgrp uucp /var/lock"
+	echo "Setting group of /var/lock to uucp"
+	chgrp uucp /var/lock
 fi
 
 #Change permissions on /var/lock to 775 if it is not 775
 if [[ ! $(stat -f %Mp%Lp /var/lock) = "0775" ]]; then
-	echo "chmod 775 /var/lock"
+	echo "Changing permissions of /var/lock"
+	chmod 775 /var/lock
 fi
 
 #add the user to the uucp group if they are not in it
 if [[ -z $(dscl . -read /Groups/_uucp GroupMembership | grep -o "$USER1") ]]; then
-	echo "dscl . -append /groups/_uucp GroupMembership $USER1"
+	echo "adding $USER1 to the uucp group"
+	dscl . -append /groups/_uucp GroupMembership $USER1
 fi
 
+echo "All done!"
 exit 0;
-	
-	
